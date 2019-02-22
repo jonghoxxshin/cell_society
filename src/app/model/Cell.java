de53b80b5e409684fe25d2e7b3dc32dda6925e30
Kyle Harvey <kph18@duke.edu>
2019-02-22 00:37:54 -0500

    added BoardTest

--- src/app/model/Cell.java
+++ src/app/model/Cell.java
@@ -78,7 +78,6 @@ public class Cell {
     //for current cell, get next state based on a given app.model.Rules object
     public int getNextState(Rules currentRules, Board board) {
         for (State state : currentRules.getPossibleStates()) {
-
             if (myState == state.getMyState()){
                 for (int[] rule : state.getRulesForState()) {
                     int actual = findNumberOfNeighborsInState(rule[1], neighbors, board);
@@ -88,7 +87,7 @@ public class Cell {
                 }
             }
         }
-        return -1;
+        return myState;
     }
 
     //set cell state
