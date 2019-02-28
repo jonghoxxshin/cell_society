91844fb60f988e14f39dcc57370a92374db9b730
Jongho Shin <js576@duke.edu>
2019-02-28 02:15:53 -0500

    new configuration still working

--- src/app/view/NewConfigView.java
+++ src/app/view/NewConfigView.java
@@ -9,11 +9,12 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 
 import java.util.ArrayList;
 
 public class NewConfigView {
-    public static final int PAGE_WIDTH = 300;
+    public static final int PAGE_WIDTH = 400;
     public static final int PAGE_HEIGHT = 500;
     public static final String TITLE = "New Configuration";
 
@@ -39,19 +40,42 @@ public class NewConfigView {
     private void setScene(){
         myRoot = new VBox();
         setComponent();
+        setDropDown();
+        setBottom();
+        myRoot.setAlignment(Pos.CENTER);
         myScene = new Scene(myRoot,PAGE_WIDTH, PAGE_HEIGHT);
     }
 
-    private void setComponent(){
+    private void setHorizontalComponent(String fieldLabel){
         HBox temp = new HBox();
-        Text tempText = new Text();
-        tempText.setText("Creator Name ? ");
-        myCreatorName = new TextField();
+        Text tempText = new Text(fieldLabel);
+        TextField tempField = new TextField();
         temp.getChildren().add(tempText);
-        temp.getChildren().add(myCreatorName);
+        temp.getChildren().add(tempField);
         temp.setAlignment(Pos.CENTER);
         myRoot.getChildren().add(temp);
     }
+
+    private void setDropDown(){
+
+
+    }
+    private void setBottom(){
+        HBox temp = new HBox();
+        Button tempButton = new Button("submit");
+        temp.getChildren().add(tempButton);
+        temp.setAlignment(Pos.CENTER);
+        myRoot.getChildren().add(temp);
+    }
+
+    private void setComponent(){
+        setHorizontalComponent("Creator's name");
+        setHorizontalComponent("Simulation name");
+        setHorizontalComponent("Description");
+    }
+
+
+
 
 
 }
