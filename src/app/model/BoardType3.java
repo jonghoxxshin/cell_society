84db81a0f273765ba839178efca353f1b90aba72
Kyle Harvey <kph18@duke.edu>
2019-03-06 19:24:30 -0500

    added Board subclasses

---
+++ src/app/model/BoardType3.java
@@ -0,0 +1,128 @@
+package app.model;
+
+import java.util.*;
+
+public class BoardType3 extends Board{
+    Cell[][] cells;
+    private int myWidth;
+    private int myHeight;
+    private String myGame;
+    private int neighborType;
+    private final int[] orderToReplace = {2, 1, 0};
+    private double threshold = 0.3;
+    private CSVParser myParser;
+    private int errorStatus;
+    private GridShapeType myGridShapeType;
+
+    public BoardType3(ResourceBundle myProperties){
+        super(myProperties);
+    }
+
+    @Override
+    public Cell[][] updateBoard(Rules rules) {
+        int maxNumDislike = getNumNeighborsToSatisfyThreshold();
+        ArrayList<Cell> satisfiedCells = new ArrayList<Cell>();
+        Stack<Cell> dissatisfiedCells = new Stack<Cell>();
+        for (int i = 0; i < myHeight; i++) {
+            for (int j = 0; j < myWidth; j++) {
+                Cell tempCell = cells[i][j];
+                int[][] neighbors = cells[i][j].getNeighbors();
+                if (tempCell.findNeighborsInState(getOppositeState(tempCell.getMyState()), neighbors, this).size() > maxNumDislike) {
+                    dissatisfiedCells.push(tempCell);
+                } else {
+                    satisfiedCells.add(tempCell);
+                }
+            }
+        }
+        Cell[][] tempCells = new Cell[myHeight][myWidth];
+        int[][] updateBoard = new int[myHeight][myWidth];
+        initializeUpdateBoard(updateBoard);
+        for (Cell satisfied : satisfiedCells) {
+            tempCells[satisfied.getMyY()][satisfied.getMyX()] = satisfied;
+            updateBoard[satisfied.getMyY()][satisfied.getMyX()] = 1;
+        }
+        shuffleStack(dissatisfiedCells);
+        for (int i = 0; i < myHeight; i++) {
+            for (int j = 0; j < myWidth; j++) {
+                if (updateBoard[i][j] == -1) {
+                    updateBoard[i][j] = 1;
+                    Cell newCell = dissatisfiedCells.pop();
+                    newCell.setMyX(j);
+                    newCell.setMyY(i);
+                    tempCells[i][j] = newCell;
+                }
+            }
+        }
+        cells = tempCells;
+        return cells;
+    }
+
+    private int getNumNeighborsToSatisfyThreshold() {
+        double maxNumOfDislikeNeighbors = 0;
+        if (neighborType == 1) {
+            maxNumOfDislikeNeighbors = threshold * 8;
+        } else if (neighborType == 2) {
+            maxNumOfDislikeNeighbors = threshold * 4;
+        }
+
+        return (int) Math.floor(maxNumOfDislikeNeighbors);
+    }
+
+    private int getOppositeState(int n) {
+        if (n == 1) {
+            return 2;
+        } else if (n == 2) {
+            return 1;
+        }
+        return 0;
+    }
+
+    private Stack<Cell> shuffleStack(Stack<Cell> pq) {
+        ArrayList<Cell> tempList = new ArrayList<Cell>();
+        while (!pq.isEmpty()) {
+            tempList.add(pq.pop());
+        }
+        shuffleList(tempList);
+        while (tempList.size() > 0) {
+            pq.push(tempList.remove(0));
+        }
+        return pq;
+    }
+
+    //SOURCE: https://www.vogella.com/tutorials/JavaAlgorithmsShuffle/article.html
+    public void shuffleList(List<Cell> a) {
+        int n = a.size();
+        Random random = new Random();
+        random.nextInt();
+        for (int i = 0; i < n; i++) {
+            int change = i + random.nextInt(n - i);
+            swap(a, i, change);
+        }
+    }
+
+    //SOURCE: https://www.vogella.com/tutorials/JavaAlgorithmsShuffle/article.html
+    private void swap(List<Cell> a, int i, int change) {
+        Cell helper = a.get(i);
+        a.set(i, a.get(change));
+        a.set(change, helper);
+    }
+
+    private int[][] initializeUpdateBoard(int[][] temp) {
+        for (int i = 0; i < myHeight; i++) {
+            for (int j = 0; j < myWidth; j++) {
+                temp[i][j] = -1;
+            }
+        }
+        return temp;
+    }
+
+    private void print2DArray(int[][] myArray) {
+        for (int[] row : myArray) {
+            for (int val : row) {
+                System.out.print(val + ",");
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ src/app/model/BoardType4.java
@@ -0,0 +1,128 @@
+package app.model;
+
+import java.util.List;
+import java.util.Random;
+import java.util.ResourceBundle;
+
+public class BoardType4 extends Board{
+    Cell[][] cells;
+    private int myWidth;
+    private int myHeight;
+    private String myGame;
+    private int neighborType;
+    private final int[] orderToReplace = {2, 1, 0};
+    private double threshold = 0.3;
+    private CSVParser myParser;
+    private int errorStatus;
+    private GridShapeType myGridShapeType;
+
+    public BoardType4(ResourceBundle myProperties){
+        super(myProperties);
+    }
+
+    @Override
+    public Cell[][] updateBoard(Rules rules) {
+        print2DBoard(cells);
+        Cell[][] tempCells = new Cell[myHeight][myWidth];
+        int[][] updateBoard = new int[myHeight][myWidth];
+        initializeUpdateBoard(updateBoard);
+        print2DArray(updateBoard);
+        for (int state : orderToReplace) {
+            for (int i = 0; i < myHeight; i++) {
+                for (int j = 0; j < myWidth; j++) {
+                    if (updateBoard[i][j] == -1) {
+                        if (cells[i][j].getMyState() == state) {
+                            Cell oldCell = cells[i][j];
+                            Cell newCell = new Cell(oldCell.getNextState(rules, this), j, i, myHeight, myWidth, neighborType, oldCell.getCurrentChronons(), oldCell.getEnergyLevel(), myGridShapeType);
+                            //check if time for reproduction
+
+
+                            //update temp cells
+                            tempCells[i][j] = newCell;
+                            updateBoard[i][j] = newCell.getMyState();
+                            int[][] neighborCoordinates = oldCell.getNeighbors();
+                            //check if need to place a shark or fish after movement
+                            if (newCell.getMyState() == 0 && oldCell.getMyState() != 0) {
+                                List<Cell> neighborCells = oldCell.findNeighborsInState(1, neighborCoordinates, this);
+                                //check if there are any fish neighbors if shark
+                                if (neighborCells.size() > 0 && oldCell.getMyState() == 2) {
+                                    if (oldCell.getMyState() == 2 && oldCell.getEnergyLevel() != 0) {
+                                        Cell cellToReplace = eatFish(neighborCells, oldCell);
+                                        tempCells[cellToReplace.getMyY()][cellToReplace.getMyX()] = cellToReplace;
+                                        updateBoard[cellToReplace.getMyY()][cellToReplace.getMyX()] = cellToReplace.getMyState();
+                                    }
+                                } else {
+                                    if (oldCell.getMyState() == 2 && oldCell.getEnergyLevel() != 0) {
+                                        Cell cellToReplace = moveSharkToEmptySpace(oldCell, newCell, neighborCoordinates);
+                                        tempCells[cellToReplace.getMyY()][cellToReplace.getMyX()] = cellToReplace;
+                                        updateBoard[cellToReplace.getMyY()][cellToReplace.getMyX()] = cellToReplace.getMyState();
+                                    } else if (oldCell.getMyState() != 2){
+                                        List<Cell> emptyNeighborCells = newCell.findNeighborsInState(0, neighborCoordinates, this);
+                                        if (emptyNeighborCells.size() >  0) {
+                                            Cell cellToReplace = emptyNeighborCells.get(getRandomIntFromBound(emptyNeighborCells.size()));
+                                            cellToReplace.setMyState(oldCell.getMyState());
+                                            tempCells[cellToReplace.getMyY()][cellToReplace.getMyX()] = cellToReplace;
+                                            updateBoard[cellToReplace.getMyY()][cellToReplace.getMyX()] = cellToReplace.getMyState();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        cells = tempCells;
+        return tempCells;
+    }
+
+    private Cell moveSharkToEmptySpace(Cell oldCell, Cell newCell, int[][] neighborCoordinates) {
+        oldCell.decrementEnergyLevel();
+        List<Cell> emptyNeighborCells = newCell.findNeighborsInState(0, neighborCoordinates, this);
+        Cell cellToReplace = emptyNeighborCells.get(getRandomIntFromBound(emptyNeighborCells.size()));
+        cellToReplace.setMyState(oldCell.getMyState());
+        cellToReplace.setCurrentEnergyLevel(oldCell.getEnergyLevel());
+        return cellToReplace;
+    }
+
+    private Cell eatFish(List<Cell> neighborCells, Cell oldCell) {
+        oldCell.decrementEnergyLevel();
+        Cell cellToReplace = neighborCells.get(getRandomIntFromBound(neighborCells.size()));
+        cellToReplace.setMyState(oldCell.getMyState());
+        cellToReplace.setCurrentChronons(oldCell.getCurrentChronons());
+        cellToReplace.setCurrentEnergyLevel(oldCell.getEnergyLevel()+10);
+        return cellToReplace;
+    }
+
+    private int[][] initializeUpdateBoard(int[][] temp) {
+        for (int i = 0; i < myHeight; i++) {
+            for (int j = 0; j < myWidth; j++) {
+                temp[i][j] = -1;
+            }
+        }
+        return temp;
+    }
+
+    private void print2DArray(int[][] myArray) {
+        for (int[] row : myArray) {
+            for (int val : row) {
+                System.out.print(val + ",");
+            }
+            System.out.println();
+        }
+    }
+
+    private void print2DBoard(Cell[][] myArray) {
+        for (Cell[] row : myArray) {
+            for (Cell val : row) {
+                System.out.print("(" + val.getMyX() + "," + val.getMyY() + ")");
+            }
+            System.out.println();
+        }
+    }
+
+    private int getRandomIntFromBound(int bound) {
+        Random newRand = new Random();
+        return newRand.nextInt(bound);
+    }
+}
