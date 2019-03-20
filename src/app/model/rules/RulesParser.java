7f298c2168c9e91914c0f84b4588a14d4ba2c346
Kyle Harvey <kph18@duke.edu>
2019-03-20 14:26:47 -0400

    added tests for board types

--- src/app/model/rules/RulesParser.java
+++ src/app/model/rules/RulesParser.java
@@ -94,9 +94,7 @@ public class RulesParser {
 
 
     private String getFileName(String game){
-        if (game.toLowerCase().equals(LIFE)){
-            return LIFE_RULES;
-        } else if (game.toLowerCase().equals(PERCOLATE)) {
+         if (game.toLowerCase().equals(PERCOLATE)) {
             return PERCOLATE_RULES;
         } else if (game.toLowerCase().equals(RPS)) {
             return RPS_RULES;
@@ -107,7 +105,7 @@ public class RulesParser {
         } else if (game.toLowerCase().equals(FIRE)) {
             return FIRE_RULES;
         }
-        return null;
+        return LIFE_RULES;
     }
 
     //Parse line with rule from file
---
+++ src/app/test/FireBoardTest.java
@@ -0,0 +1,13 @@
+package app.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FireBoardTest {
+
+    @Test
+    void updateBoardTest() {
+
+    }
+}
